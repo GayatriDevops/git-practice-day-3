15e6d1890296a65b767c73d8e0617bbb85e66825
root <root@ip-172-31-29-37.ap-southeast-1.compute.internal>
2025-04-09 16:56:33 +0000

    image id changed by linux

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
 
     # Specify the EC2 instance parameters
     instance_params = {
-        'ImageId': 'ami-0c55b159cbfafe1f0',  # Example AMI ID (Amazon Linux 2)
+        'ImageId': 'ami-0c55b159',  # Example AMI ID (Amazon Linux 2)
         'InstanceType': 't2.micro',  # Instance type
         'MinCount': 2,  # Minimum number of instances to launch
         'MaxCount': 1,  # Maximum number of instances to launch
